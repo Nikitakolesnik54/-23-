02ecd22596e5d59a7eb6763ac87a839d851cac78
IA_2022 <IA_2022@lspk.ru>
2024-04-26 12:30:38 +0300

    Добавьте файлы проекта.

---
+++ ПЗ 23 по моделированию/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПЗ_23_по_моделированию
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
